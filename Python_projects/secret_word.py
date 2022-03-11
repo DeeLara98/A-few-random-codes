ba94768ac80fe0c6b5ef93dcd5380d082fd55559
DeeLara98 <72470622+DeeLara98@users.noreply.github.com>
2022-03-10 18:20:51 -0800

    Create secret_word.py

---
+++ Python_projects/secret_word.py
@@ -0,0 +1,26 @@
+def secret_number():
+    secret = random.randint(1, 100)
+    guessed = False
+    attempts = 10
+    print('I am thinking of a number between 1 and 100...')
+    prompt = 'Enter a number: '
+    answer = int(input(prompt))
+    while answer != secret and attempts > 0:
+        attempts -= 1
+        print('Sorry, that is incorrect. You have {} attempts left'.format(attempts))
+        if answer > secret:
+            print('Hint: try thinking lower.')
+        elif answer < secret:
+            print('Hint: try thinking higher')
+        answer = int(input(prompt))
+    if attempts == 0:
+        print('Sorry, you have ran out of attempts. The secret number is {}'.format(secret))
+    if answer == secret:
+        print('Congrats, you have won! The secret number was {}'.format(secret))
+        guessed = True
+    play_again = 'Do you want to play again? Y/N '
+    play_again_answer = input(play_again)
+    if play_again_answer == 'Y':
+        secret_number()
+    else:
+        print('Alrighty. Until next time!')
